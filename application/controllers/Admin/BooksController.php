0a7dd3b304a387798bc70651cadfa467180c7f8d
Aleksandar Stevanovic <alexsandarrr@gmail.com>
2016-09-05 22:11:32 +0200

    05.09.

--- application/controllers/Admin/BooksController.php
+++ application/controllers/Admin/BooksController.php
@@ -3,7 +3,28 @@
 class Admin_BooksController extends Zend_Controller_Action
 {
 	public function indexAction () {
-		
+            $flashMessenger = $this->getHelper('FlashMessenger');
+        
+            $systemMessages = array(
+                'success' => $flashMessenger->getMessages('success'),
+                'errors' => $flashMessenger->getMessages('errors'),
+            );
+            
+            $cmsMembersDbTable = new Application_Model_DbTable_CmsMembers();
+        
+            $members = $cmsMembersDbTable->search(array(
+    //            'filters' => array(
+    //                'id' => array(1, 3, 5, 7)
+    //            ),
+                'orders' => array(
+                    'order_number' => 'ASC'
+                ),
+                //'limit' => 4,
+                //'page' => 3
+            ));
+
+            $this->view->members = $members;
+            $this->view->systemMessages = $systemMessages;
 	}
 	
 	public function addAction () {
